951e42b349d5fb7ada7aa417b73f16ff48d27fc1
KacperAa <kacperaugvstyn@gmail.com>
2024-04-15 20:07:12 +0200

    ref

--- projects/chat-app/src/app/app.component.ts
+++ projects/chat-app/src/app/app.component.ts
@@ -1,11 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'kaa-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  @Input('dd') public testd!: string;
-  title = 'chat-app';
-}
+export class AppComponent {}
