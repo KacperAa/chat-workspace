c2d9e898057225afc8739820d5e89dd80acd3d73
KacperAa <kacperaugvstyn@gmail.com>
2024-04-15 19:12:37 +0200

    configure project

---
+++ projects/ui/src/lib/ui.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ui-ui',
+  standalone: true,
+  imports: [],
+  template: ` <p>ui works!</p> `,
+  styles: ``,
+})
+export class UiComponent {}
