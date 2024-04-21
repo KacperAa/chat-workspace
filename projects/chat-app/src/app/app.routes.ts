689831a88dde8aa72bbc420858041e732333ab3e
KacperAa <kacperaugvstyn@gmail.com>
2024-04-21 12:51:36 +0200

    implement side nav

--- projects/chat-app/src/app/app.routes.ts
+++ projects/chat-app/src/app/app.routes.ts
@@ -1,5 +1,8 @@
+import { AuthComponent } from './presentation/access/auth/auth.component';
+import { CoreComponent } from './presentation/chat-app-presentation/core/core.component';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  // {path: 'chat', component:}
+  { path: 'login', component: AuthComponent },
+  { path: 'chat', component: CoreComponent },
 ];
