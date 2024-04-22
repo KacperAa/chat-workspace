8519a159637cee635311d9abfd54ae9a9d196ebe
KacperAa <kacperaugvstyn@gmail.com>
2024-04-22 19:40:33 +0200

    create filter users component

---
+++ projects/chat-app/src/app/presentation/chat-app-presentation/pages/chat/feature/filter-users/filter-users.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kaa-filter-users',
+  standalone: true,
+  imports: [],
+  templateUrl: './filter-users.component.html',
+  styleUrl: './filter-users.component.scss'
+})
+export class FilterUsersComponent {
+
+}
--- projects/chat-app/src/app/presentation/chat-app-presentation/pages/chat/main/chat.component.ts
+++ projects/chat-app/src/app/presentation/chat-app-presentation/pages/chat/main/chat.component.ts
@@ -1,10 +1,11 @@
 import { CoreComponent } from '../../../core/core.component';
+import { FilterUsersComponent } from '../feature/filter-users/filter-users.component';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'kaa-chat',
   standalone: true,
-  imports: [CoreComponent],
+  imports: [CoreComponent, FilterUsersComponent],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss',
 })
