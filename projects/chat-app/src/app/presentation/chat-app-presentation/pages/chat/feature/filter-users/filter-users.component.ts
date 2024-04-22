c10fe32f249e87c321770eefa25e6eb297ea7ff4
KacperAa <kacperaugvstyn@gmail.com>
2024-04-22 19:49:33 +0200

    create filter component

--- projects/chat-app/src/app/presentation/chat-app-presentation/pages/chat/feature/filter-users/filter-users.component.ts
+++ projects/chat-app/src/app/presentation/chat-app-presentation/pages/chat/feature/filter-users/filter-users.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+
+const MATERIAL_IMPORTS = [
+  MatButtonModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatInputModule,
+];
 
 @Component({
   selector: 'kaa-filter-users',
   standalone: true,
-  imports: [],
+  imports: [MATERIAL_IMPORTS, FormsModule, ReactiveFormsModule],
   templateUrl: './filter-users.component.html',
-  styleUrl: './filter-users.component.scss'
+  styleUrl: './filter-users.component.scss',
 })
 export class FilterUsersComponent {
-
+  public searchUserControl = new FormControl<string>('');
 }
