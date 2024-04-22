5e451e31c303a97a7c4e275104162be2f60ea40d
KacperAa <kacperaugvstyn@gmail.com>
2024-04-22 20:07:52 +0200

    init user cicrle component

--- projects/chat-app/src/app/presentation/chat-app-presentation/pages/chat/main/chat.component.ts
+++ projects/chat-app/src/app/presentation/chat-app-presentation/pages/chat/main/chat.component.ts
@@ -1,11 +1,12 @@
 import { CoreComponent } from '../../../core/core.component';
 import { FilterUsersComponent } from '../feature/filter-users/filter-users.component';
+import { UserCircleComponent } from '../ui/user-circle/user-circle.component';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'kaa-chat',
   standalone: true,
-  imports: [CoreComponent, FilterUsersComponent],
+  imports: [CoreComponent, FilterUsersComponent, UserCircleComponent],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss',
 })
---
+++ projects/chat-app/src/app/presentation/chat-app-presentation/pages/chat/ui/user-circle/user-circle.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kaa-user-circle',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-circle.component.html',
+  styleUrl: './user-circle.component.scss'
+})
+export class UserCircleComponent {
+
+}
