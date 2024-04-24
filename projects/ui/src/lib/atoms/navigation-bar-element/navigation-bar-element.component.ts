8589c1657b2ad2ebdac5b609a34452abc5815dd6
KacperAa <kacperaugvstyn@gmail.com>
2024-04-24 19:45:06 +0200

    finish nav bar element

--- projects/ui/src/lib/atoms/navigation-bar-element/navigation-bar-element.component.ts
+++ projects/ui/src/lib/atoms/navigation-bar-element/navigation-bar-element.component.ts
@@ -1,13 +1,30 @@
-import { Component, input } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewEncapsulation, inject, input } from '@angular/core';
 
 @Component({
-  selector: 'ui-navigation-bar-element',
+  selector: 'a[ui-nav-bar-element]',
   standalone: true,
   imports: [],
   templateUrl: './navigation-bar-element.component.html',
   styleUrl: './navigation-bar-element.component.scss',
+  encapsulation: ViewEncapsulation.None,
 })
-export class NavigationBarElementComponent {
+export class NavigationBarElementComponent implements OnInit {
+  public elementRef = inject(ElementRef);
+
   public isActivated = input<boolean>(false);
-  public href = input.required<string>();
+
+  constructor() {
+    const element = this.elementRef.nativeElement;
+    const classList = (element as HTMLElement).classList;
+
+    classList.add('nav-bar-element');
+  }
+  public ngOnInit(): void {
+    if (this.isActivated()) {
+      const element = this.elementRef.nativeElement;
+      const classList = (element as HTMLElement).classList;
+
+      classList.add('active');
+    }
+  }
 }
