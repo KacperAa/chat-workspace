6364cc850277e8331422bb78cc98d863a36be5d2
KacperAa <kacperaugvstyn@gmail.com>
2024-04-25 17:21:49 +0200

    refactor avatar to content projection

--- projects/chat-app/src/app/presentation/chat-app-presentation/pages/chat/feature/conversations-list-section/conversations-list-section.component.ts
+++ projects/chat-app/src/app/presentation/chat-app-presentation/pages/chat/feature/conversations-list-section/conversations-list-section.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 import { AvatarWithContentComponent } from '@ui/AvatarWithContentComponent';
+import { AvatarComponent } from '@ui/AvatarWithContentComponent';
 
 @Component({
   selector: 'kaa-conversations-list-section',
   standalone: true,
-  imports: [AvatarWithContentComponent],
+  imports: [AvatarWithContentComponent, AvatarComponent],
   templateUrl: './conversations-list-section.component.html',
   styleUrl: './conversations-list-section.component.scss',
 })
--- projects/chat-app/src/app/presentation/chat-app-presentation/pages/chat/ui/molecules/user-carousel/users-horizontal-scroller.component.ts
+++ projects/chat-app/src/app/presentation/chat-app-presentation/pages/chat/ui/molecules/user-carousel/users-horizontal-scroller.component.ts
@@ -1,12 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { AvatarComponent } from '@ui/AvatarComponent';
 import { AvatarWithContentComponent } from '@ui/AvatarWithContentComponent';
 import { HorizontalScrollComponent } from '@ui/HorizontalScrollComponent';
 
 @Component({
   selector: 'kaa-users-horizontal-scroller',
   standalone: true,
-  imports: [AvatarWithContentComponent, HorizontalScrollComponent],
+  imports: [AvatarWithContentComponent, HorizontalScrollComponent, AvatarComponent],
   templateUrl: './users-horizontal-scroller.component.html',
   styleUrl: './users-horizontal-scroller.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
