657881b7a7dca7aab9bfd5d1393a00088612c097
KacperAa <kacperaugvstyn@gmail.com>
2024-04-25 17:44:51 +0200

    extand avatar component

--- projects/ui/src/lib/atoms/avatar/avatar.component.ts
+++ projects/ui/src/lib/atoms/avatar/avatar.component.ts
@@ -1,4 +1,14 @@
-import { ChangeDetectionStrategy, Component, ElementRef, ViewEncapsulation, inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewEncapsulation,
+  inject,
+  input,
+} from '@angular/core';
+
+import { AvatarSize } from './models/avatar-size.model';
 
 @Component({
   selector: 'img[ui-avatar]',
@@ -9,7 +19,9 @@ import { ChangeDetectionStrategy, Component, ElementRef, ViewEncapsulation, inje
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AvatarComponent {
+export class AvatarComponent implements OnInit {
+  public readonly size = input<AvatarSize>('normal');
+
   public elementRef = inject(ElementRef);
 
   constructor() {
@@ -17,5 +29,31 @@ export class AvatarComponent {
     const classList = element.classList;
 
     classList.add('user-circle', 'mat-elevation-z10');
+  }
+
+  public ngOnInit(): void {
+    this._setAvatarSize();
+  }
+
+  private _setAvatarSize(): void {
+    const element = this.elementRef.nativeElement as HTMLElement;
+    const classList = element.classList;
+    switch (this.size()) {
+      case 'small': {
+        classList.add('user-circle-sm');
+        break;
+      }
+      case 'normal': {
+        classList.add('user-circle-md');
+        break;
+      }
+      case 'large': {
+        classList.add('user-circle-lg');
+        break;
+      }
+      default: {
+        let n: never;
+      }
+    }
   }
 }
