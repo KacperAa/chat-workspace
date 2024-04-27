2fd8cf376ba2f3af15bce247097683f86ac4ef77
KacperAa <kacperaugvstyn@gmail.com>
2024-04-27 19:58:14 +0200

    finish transform logic

--- projects/ui/src/lib/atoms/skeleton-loader/skeleton-bar/skeleton-bar.component.ts
+++ projects/ui/src/lib/atoms/skeleton-loader/skeleton-bar/skeleton-bar.component.ts
@@ -8,6 +8,16 @@ import { Component, input } from '@angular/core';
   styleUrls: ['./skeleton-bar.component.scss', '../styles/_skeleton-base.scss'],
 })
 export class SkeletonBarComponent {
-  public readonly width = input('20rem');
+  public readonly width = input('20rem', {
+    transform: (value: string | number) => {
+      const isNumber = typeof value === 'number';
+      if (isNumber) {
+        return `${value}rem`;
+      } else if (!isNumber && !value.endsWith('rem')) {
+        return `${value}rem`;
+      }
+      return value;
+    },
+  });
   public readonly height = input('1.5rem');
 }
