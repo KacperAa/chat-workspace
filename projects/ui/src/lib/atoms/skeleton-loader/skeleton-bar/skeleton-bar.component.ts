6c0b8d2526b47204d74b8ac529d895463afdc9a1
KacperAa <kacperaugvstyn@gmail.com>
2024-04-27 19:35:22 +0200

    refactor file position

--- projects/ui/src/lib/atoms/skeleton-loader/skeleton-bar/skeleton-bar.component.ts
+++ projects/ui/src/lib/atoms/skeleton-loader/skeleton-bar/skeleton-bar.component.ts
@@ -5,6 +5,6 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './skeleton-bar.component.html',
-  styleUrls: ['./skeleton-bar.component.scss', '../../styles/_skeleton-base.scss'],
+  styleUrls: ['./skeleton-bar.component.scss', '../styles/_skeleton-base.scss'],
 })
 export class SkeletonBarComponent {}
--- projects/ui/src/public-api.ts
+++ projects/ui/src/public-api.ts
@@ -2,6 +2,11 @@
  * Public API Surface of ui
  */
 
-export { AvatarComponent, NavigationBarElementComponent, SkeletonCircleLoaderComponent } from './lib/atoms';
+export {
+  AvatarComponent,
+  NavigationBarElementComponent,
+  SkeletonCircleLoaderComponent,
+  SkeletonBarComponent,
+} from './lib/atoms';
 export { AvatarWithContentComponent } from './lib/molecules';
 export { HorizontalScrollComponent } from './lib/organisms';
