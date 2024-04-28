75b413f33be50f00406ce625aedb5f3b4a14a5e7
KacperAa <kacperaugvstyn@gmail.com>
2024-04-28 16:22:14 +0200

    add imports

--- projects/chat-app/src/app/presentation/chat-app-presentation/pages/chat/feature/user-conversation/user-conversation.component.ts
+++ projects/chat-app/src/app/presentation/chat-app-presentation/pages/chat/feature/user-conversation/user-conversation.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { AvatarWithContentComponent } from '@ui/AvatarWithContentComponent';
 
 @Component({
   selector: 'kaa-user-conversation',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule, AvatarWithContentComponent, MatIcon],
   templateUrl: './user-conversation.component.html',
-  styleUrl: './user-conversation.component.scss'
+  styleUrl: './user-conversation.component.scss',
 })
-export class UserConversationComponent {
-
-}
+export class UserConversationComponent {}
