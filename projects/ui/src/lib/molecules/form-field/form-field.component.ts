482ae78a0acc4cc6064542ed5820bd20f36f7f1e
KacperAa <kacperaugvstyn@gmail.com>
2024-04-28 18:48:12 +0200

    finish input component

--- projects/ui/src/lib/molecules/form-field/form-field.component.ts
+++ projects/ui/src/lib/molecules/form-field/form-field.component.ts
@@ -1,4 +1,11 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  ViewEncapsulation,
+  inject,
+} from '@angular/core';
 
 @Component({
   selector: 'ui-form-field',
@@ -7,5 +14,21 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './form-field.component.html',
   styleUrl: './form-field.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
 })
-export class FormFieldComponent {}
+export class FormFieldComponent implements AfterViewInit {
+  public elementRef = inject(ElementRef);
+
+  public ngAfterViewInit(): void {
+    this._addPaddingInInputWhenPreffix();
+  }
+
+  private _addPaddingInInputWhenPreffix(): void {
+    const preffixElement = this.elementRef.nativeElement.querySelector('[preffix]') as HTMLElement;
+    const inputElement = this.elementRef.nativeElement.querySelector('[ui-input]') as HTMLInputElement;
+
+    if (preffixElement) {
+      inputElement.classList.add('padding-left-when-preffix');
+    }
+  }
+}
