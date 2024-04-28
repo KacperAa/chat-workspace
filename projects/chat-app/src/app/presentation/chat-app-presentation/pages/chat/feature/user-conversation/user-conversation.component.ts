3b64ce7040c84228a1aa4c57dee2b4eac2e24046
KacperAa <kacperaugvstyn@gmail.com>
2024-04-28 16:25:48 +0200

    implement user circle component

--- projects/chat-app/src/app/presentation/chat-app-presentation/pages/chat/feature/user-conversation/user-conversation.component.ts
+++ projects/chat-app/src/app/presentation/chat-app-presentation/pages/chat/feature/user-conversation/user-conversation.component.ts
@@ -3,12 +3,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
+import { AvatarComponent } from '@ui/AvatarComponent';
 import { AvatarWithContentComponent } from '@ui/AvatarWithContentComponent';
 
 @Component({
   selector: 'kaa-user-conversation',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, AvatarWithContentComponent, MatIcon],
+  imports: [MatToolbarModule, MatButtonModule, AvatarWithContentComponent, MatIcon, AvatarComponent],
   templateUrl: './user-conversation.component.html',
   styleUrl: './user-conversation.component.scss',
 })
