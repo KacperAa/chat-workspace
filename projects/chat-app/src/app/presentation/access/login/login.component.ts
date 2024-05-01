b1e40cf1be82c788c9eae21b117dd76f0999080b
KacperAa <kacperaugvstyn@gmail.com>
2024-05-01 21:03:53 +0200

    create reusable auth template

--- projects/chat-app/src/app/presentation/access/login/login.component.ts
+++ projects/chat-app/src/app/presentation/access/login/login.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { MatFormField } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
 
 import { AuthCoreComponent } from '../ui/templates/auth-core/auth-core.component';
 
 @Component({
   selector: 'kaa-login',
   standalone: true,
-  imports: [AuthCoreComponent],
+  imports: [AuthCoreComponent, MatFormField, MatInput],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
