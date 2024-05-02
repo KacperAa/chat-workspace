797449ccada4ef7e7827f20948a8b4b92141f3f2
KacperAa <kacperaugvstyn@gmail.com>
2024-05-02 18:12:57 +0200

    remove useful import

--- projects/chat-app/src/app/presentation/access/register/register.component.ts
+++ projects/chat-app/src/app/presentation/access/register/register.component.ts
@@ -3,14 +3,12 @@ import { MatButton } from '@angular/material/button';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 
-import { VerticalScrollComponent } from '@ui/VerticalScrollComponent';
-
 import { AuthCoreComponent } from '../ui/templates/auth-core/auth-core.component';
 
 @Component({
   selector: 'kaa-register',
   standalone: true,
-  imports: [AuthCoreComponent, MatButton, MatLabel, MatFormField, MatInput, VerticalScrollComponent],
+  imports: [AuthCoreComponent, MatButton, MatLabel, MatFormField, MatInput],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
