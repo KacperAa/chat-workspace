f36998ff7b712985cdd06580bcabaffb177203ae
KacperAa <kacperaugvstyn@gmail.com>
2024-05-02 18:32:17 +0200

    create interface

--- projects/chat-app/src/app/presentation/access/login/login.component.ts
+++ projects/chat-app/src/app/presentation/access/login/login.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatButton } from '@angular/material/button';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 
 import { AuthCoreComponent } from '../ui/templates/auth-core/auth-core.component';
+import { LoginFormGroup } from './models/login-form-group.model';
+
+const MATERIAL_IMPORTS = [MatFormField, MatInput, MatLabel, MatButton];
 
 @Component({
   selector: 'kaa-login',
   standalone: true,
-  imports: [AuthCoreComponent, MatFormField, MatInput, MatLabel, MatButton],
+  imports: [MATERIAL_IMPORTS, AuthCoreComponent, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  private _fb = inject(FormBuilder);
+  public loginFormGroup: FormGroup<LoginFormGroup> = this._fb.group({
+    email: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+  });
+}
