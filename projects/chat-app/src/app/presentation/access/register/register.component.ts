e2dde9c2aa3a7e8d04d4f814ce997f8570bf17fb
KacperAa <kacperaugvstyn@gmail.com>
2024-05-02 15:22:43 +0200

    implement core to register page

--- projects/chat-app/src/app/presentation/access/register/register.component.ts
+++ projects/chat-app/src/app/presentation/access/register/register.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+
+import { AuthCoreComponent } from '../ui/templates/auth-core/auth-core.component';
 
 @Component({
   selector: 'kaa-register',
   standalone: true,
-  imports: [],
+  imports: [AuthCoreComponent, MatButton, MatLabel, MatFormField, MatInput],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
-export class RegisterComponent {
-
-}
+export class RegisterComponent {}
