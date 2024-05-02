92f04a48dd6b1db504acfb8565b0272ca883ccc4
KacperAa <kacperaugvstyn@gmail.com>
2024-05-02 18:12:18 +0200

    finish core

--- projects/chat-app/src/app/presentation/access/register/register.component.ts
+++ projects/chat-app/src/app/presentation/access/register/register.component.ts
@@ -3,12 +3,14 @@ import { MatButton } from '@angular/material/button';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 
+import { VerticalScrollComponent } from '@ui/VerticalScrollComponent';
+
 import { AuthCoreComponent } from '../ui/templates/auth-core/auth-core.component';
 
 @Component({
   selector: 'kaa-register',
   standalone: true,
-  imports: [AuthCoreComponent, MatButton, MatLabel, MatFormField, MatInput],
+  imports: [AuthCoreComponent, MatButton, MatLabel, MatFormField, MatInput, VerticalScrollComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
