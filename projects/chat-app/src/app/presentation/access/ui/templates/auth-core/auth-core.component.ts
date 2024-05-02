cde4b89f3579ecc78ff09a99d0b82fd4ce51a5bc
KacperAa <kacperaugvstyn@gmail.com>
2024-05-02 14:44:13 +0200

    add title to reusable core

--- projects/chat-app/src/app/presentation/access/ui/templates/auth-core/auth-core.component.ts
+++ projects/chat-app/src/app/presentation/access/ui/templates/auth-core/auth-core.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'kaa-auth-core',
   standalone: true,
   imports: [],
   templateUrl: './auth-core.component.html',
-  styleUrl: './auth-core.component.scss'
+  styleUrl: './auth-core.component.scss',
 })
 export class AuthCoreComponent {
-
+  public headerText = input.required<string>();
 }
