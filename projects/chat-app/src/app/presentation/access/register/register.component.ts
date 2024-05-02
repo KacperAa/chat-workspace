74397e5e870020d703c1f9c2497392fb69bddc4c
KacperAa <kacperaugvstyn@gmail.com>
2024-05-02 18:38:53 +0200

    add register form group module

--- projects/chat-app/src/app/presentation/access/register/register.component.ts
+++ projects/chat-app/src/app/presentation/access/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatButton } from '@angular/material/button';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
@@ -7,10 +8,12 @@ import { MatInput } from '@angular/material/input';
 import { AuthCoreComponent } from '../ui/templates/auth-core/auth-core.component';
 import { RegisterFormGroup } from './models/register-form-group.model';
 
+const MATERIAL_IMPORTS = [MatButton, MatLabel, MatFormField, MatInput];
+
 @Component({
   selector: 'kaa-register',
   standalone: true,
-  imports: [AuthCoreComponent, MatButton, MatLabel, MatFormField, MatInput],
+  imports: [MATERIAL_IMPORTS, AuthCoreComponent, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
