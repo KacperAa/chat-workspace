a69418c59ad16fac894676da238e0af1f7d9d4ec
KacperAa <kacperaugvstyn@gmail.com>
2024-05-15 20:38:37 +0200

    instal chat api

--- projects/chat-app/src/app/app.component.ts
+++ projects/chat-app/src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { StreamAutocompleteTextareaModule, StreamChatModule } from 'stream-chat-angular';
 
 import { CoreComponent } from './presentation/chat-app-presentation/core/core.component';
+
+const STREAM_CHAT_IMPORTS = [StreamAutocompleteTextareaModule, StreamChatModule];
 
 @Component({
   selector: 'kaa-root',
   standalone: true,
-  imports: [RouterOutlet, CoreComponent],
+  imports: [RouterOutlet, CoreComponent, TranslateModule, STREAM_CHAT_IMPORTS],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
