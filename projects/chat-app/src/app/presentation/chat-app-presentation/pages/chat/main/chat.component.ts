047e65c7f1f69fdcfda62a80c927d85d33d21fd2
KacperAa <kacperaugvstyn@gmail.com>
2024-05-15 21:00:56 +0200

    create env

--- projects/chat-app/src/app/presentation/chat-app-presentation/pages/chat/main/chat.component.ts
+++ projects/chat-app/src/app/presentation/chat-app-presentation/pages/chat/main/chat.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ChannelService, ChatClientService, StreamI18nService } from 'stream-chat-angular';
 
 import { AvatarWithContentComponent } from '@ui/AvatarWithContentComponent';
 import { NavigationBarElementComponent } from '@ui/NavigationBarElementComponent';
 import { SkeletonCircleAndBarsComponent } from '@ui/SkeletonCircleAndBarsComponent';
 
+import { environment } from '../../../../../../environments/environment';
+import { AuthStore } from '../../../../../business/api/auth/auth.store';
 import { UserHttpService } from '../../../../../business/api/user/user-http.service';
 import { CoreComponent } from '../../../core/core.component';
 import { ConversationsListSectionComponent } from '../feature/conversations-list-section/conversations-list-section.component';
@@ -30,11 +33,18 @@ import { ChatFacade } from './chat-facade';
   styleUrl: './chat.component.scss',
 })
 export class ChatComponent implements OnInit {
-  public chatFacade = inject(ChatFacade);
+  protected chatFacade = inject(ChatFacade);
+  private _auth = inject(AuthStore);
 
   private _usersHttp = inject(UserHttpService);
+  private _chatService: ChatClientService = inject(ChatClientService);
+  private _channelService: ChannelService = inject(ChannelService);
+  private _streamI18nService: StreamI18nService = inject(StreamI18nService);
 
   public ngOnInit(): void {
     this._usersHttp.getUsers().subscribe();
+
+    this._chatService.init(environment.stream.key, this._auth.loggedUser()?.uid, this._auth.loggedUser()?.getIdToken);
+    this._streamI18nService.setTranslation();
   }
 }
---
+++ projects/chat-app/src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  stream: {
+    key: 'yveygeypmkdb',
+  },
+};
