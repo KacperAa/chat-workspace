91c2c6a8bd52e03b6be17f31918e067d483e26c6
KacperAa <kacperaugvstyn@gmail.com>
2024-05-28 09:58:36 +0200

    implement data

--- projects/chat-app/src/app/presentation/chat-app-presentation/pages/chat/feature/conversation-window/conversation-window.component.ts
+++ projects/chat-app/src/app/presentation/chat-app-presentation/pages/chat/feature/conversation-window/conversation-window.component.ts
@@ -1,10 +1,12 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 import { ChannelsStore } from '../../../../../../business/api/channels/channels.store';
 import { ConversationData } from '../../../../../../business/api/channels/models/conversation-data.model';
 import { MessagesCollectionComponent } from '../../ui/organisms/messages-collection/messages-collection.component';
 import { ConversationCoreComponent } from '../../ui/templates/conversation-core/conversation-core.component';
 
+import { toSignal } from '@angular/core/rxjs-interop';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -15,8 +17,21 @@ import { Observable } from 'rxjs';
   styleUrl: './conversation-window.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ConversationWindowComponent {
+export class ConversationWindowComponent implements OnInit {
   private _channelStore = inject(ChannelsStore);
+  private _activatedRoute = inject(ActivatedRoute);
 
-  public channelData!: Observable<ConversationData>;
+  private _channelData!: Observable<ConversationData>;
+  public channelData = toSignal(this._channelData, { requireSync: true });
+
+  public ngOnInit(): void {
+    this._getChannelData();
+  }
+
+  private _getChannelData(): void {
+    this._activatedRoute.params.subscribe((params: Params) => {
+      const channelId = params['id'];
+      this._channelData = this._channelStore.getChannel(channelId);
+    });
+  }
 }
