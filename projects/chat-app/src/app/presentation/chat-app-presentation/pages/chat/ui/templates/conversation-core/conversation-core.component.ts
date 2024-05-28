2c7b908f21c40dbbbffd5cc53e17395eb9cdc161
KacperAa <kacperaugvstyn@gmail.com>
2024-05-28 19:48:52 +0200

    refactor styles

--- projects/chat-app/src/app/presentation/chat-app-presentation/pages/chat/ui/templates/conversation-core/conversation-core.component.ts
+++ projects/chat-app/src/app/presentation/chat-app-presentation/pages/chat/ui/templates/conversation-core/conversation-core.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, ElementRef, Signal, input, output, viewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -35,8 +35,16 @@ export class ConversationCoreComponent {
   public conversationPresentationData = input.required<ConversationData | null>();
   public onWrite = output<void>();
 
-  public messageInputRef: Signal<ElementRef<HTMLInputElement>> = viewChild.required('messageInputRef');
+  public isInputFocusMode: boolean = false;
 
   public topActionButtons: ActionButton[] = [{ icon: 'phone' }, { icon: 'videocam' }, { icon: 'info' }];
   public bottomActionButtons: ActionButton[] = [{ icon: 'photo' }, { icon: 'microphone' }, { icon: 'photo_camera' }];
+
+  public onInputFocus(): void {
+    this.isInputFocusMode = true;
+  }
+
+  public onInputBlur(): void {
+    this.isInputFocusMode = false;
+  }
 }
