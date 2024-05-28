5313f7ee1089df6a4ed76f6e7bc3ede31ef45c89
KacperAa <kacperaugvstyn@gmail.com>
2024-05-28 15:52:02 +0200

    remove useful code block

--- projects/chat-app/src/app/business/api/channels/channels-api.service.ts
+++ projects/chat-app/src/app/business/api/channels/channels-api.service.ts
@@ -10,35 +10,6 @@ import { Observable, filter, map, shareReplay } from 'rxjs';
 export class ChannelsApiService {
   private _channelService = inject(ChannelService);
 
-  // public mappedChannelsData: Signal<ChannelListElement[] | undefined> = toSignal(
-  //   this._channelService.channels$.pipe(
-  //     filter((channels): channels is Channel<DefaultStreamChatGenerics>[] => !!channels),
-  //     map(channels =>
-  //       channels.map(channel => ({
-  //         id: String(channel.id),
-  //         name: String(channel.data?.name ?? ''),
-  //         image: String(channel.data?.image ?? ''),
-  //         last_message_at: String(channel.data?.last_message_at ?? ''),
-  //       }))
-  //     )
-  //   )
-  // );
-
-  // public getChannel(id: string): Observable<ConversationData> {
-  //   return this.getChannelApi(id).pipe(
-  //     map(channel => {
-  //       this._channelService.setAsActiveChannel(channel);
-  //       return {
-  //         id: String(channel.id),
-  //         name: String(channel.data?.name ?? ''),
-  //         image: String(channel.data?.image ?? ''),
-  //         last_message_at: String(channel.data?.last_message_at ?? ''),
-  //         messageSets: channel.state.messageSets,
-  //       };
-  //     })
-  //   );
-  // }
-
   public getChannelApi(id: string): Observable<Channel<DefaultStreamChatGenerics>> {
     return this._channelService.channels$.pipe(
       shareReplay(),
