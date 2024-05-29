63460e59fd86a9af73c19c5161881225e47a02b0
KacperAa <kacperaugvstyn@gmail.com>
2024-05-29 15:18:09 +0200

    ref

--- projects/ui/src/lib/molecules/avatar-with-icon-button/avatar-with-icon-button.component.ts
+++ projects/ui/src/lib/molecules/avatar-with-icon-button/avatar-with-icon-button.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 
 import { CircleSizeService } from '../../atoms/circle-size/circle-size.service';
@@ -13,5 +13,8 @@ import { CircleSizeService } from '../../atoms/circle-size/circle-size.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AvatarWithIconButtonComponent {
-  public onClickIcon = output<void>();
+  public borderColorVar = input('');
+  public backgroundColorVar = input('');
+
+  public onClickBtn = output<void>();
 }
