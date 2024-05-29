d81f9cc4bad0ef9b3c87faf0269386510c58192e
KacperAa <kacperaugvstyn@gmail.com>
2024-05-29 13:05:59 +0200

    navigate to chat function

--- projects/chat-app/src/app/presentation/chat-app-presentation/pages/settings/main/settings/settings.component.ts
+++ projects/chat-app/src/app/presentation/chat-app-presentation/pages/settings/main/settings/settings.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatIconButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatToolbar } from '@angular/material/toolbar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'kaa-settings',
   standalone: true,
-  imports: [],
+  imports: [MatToolbar, MatIconButton, MatIcon],
   templateUrl: './settings.component.html',
-  styleUrl: './settings.component.scss'
+  styleUrl: './settings.component.scss',
 })
 export class SettingsComponent {
+  private _router: Router = inject(Router);
 
+  public navigateToChat(): void {
+    this._router.navigate(['chat']);
+  }
 }
