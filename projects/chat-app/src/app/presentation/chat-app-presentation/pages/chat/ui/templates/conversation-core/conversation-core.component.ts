689c33dc477455986b679eb1b92af3184d88a95e
KacperAa <kacperaugvstyn@gmail.com>
2024-05-29 12:18:58 +0200

    refactor

--- projects/chat-app/src/app/presentation/chat-app-presentation/pages/chat/ui/templates/conversation-core/conversation-core.component.ts
+++ projects/chat-app/src/app/presentation/chat-app-presentation/pages/chat/ui/templates/conversation-core/conversation-core.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnInit, input, output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
@@ -30,39 +30,26 @@ import { ActionButton } from './models/action-button.model';
   styleUrl: './conversation-core.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ConversationCoreComponent implements OnInit {
+export class ConversationCoreComponent {
   public conversationPresentationData = input.required<ConversationData | null>();
   public onWrite = output<void>();
 
-  public messageFormControl = new FormControl<string | null>('', [Validators.maxLength(1000)]);
+  public messageFormControl = new FormControl<string | null>('', [
+    Validators.required,
+    Validators.minLength(1),
+    Validators.maxLength(1000),
+  ]);
 
   public isInputFocusMode: boolean = false;
-  public isSendButtonVisible: boolean = false;
 
   public topActionButtons: ActionButton[] = [{ icon: 'phone' }, { icon: 'videocam' }, { icon: 'info' }];
   public bottomActionButtons: ActionButton[] = [{ icon: 'photo' }, { icon: 'microphone' }, { icon: 'photo_camera' }];
 
-  public ngOnInit(): void {
-    this._trackIsSendButtonVisible();
-  }
-
   public onSendMessage(): void {
-    if (this._isMessageCanSend()) {
+    const isFormValid: boolean = this.messageFormControl.valid;
+    if (isFormValid) {
       console.log(this.messageFormControl.value);
       this.messageFormControl.reset();
     }
-  }
-
-  public _trackIsSendButtonVisible(): void {
-    this.messageFormControl.valueChanges.subscribe(value => {
-      this.isSendButtonVisible = this.isInputFocusMode && value?.length! > 0;
-    });
-  }
-
-  private _isMessageCanSend(): boolean {
-    const isFormValid: boolean = this.messageFormControl.valid;
-    const message: string | null = this.messageFormControl.value;
-
-    return message !== null && isFormValid;
   }
 }
