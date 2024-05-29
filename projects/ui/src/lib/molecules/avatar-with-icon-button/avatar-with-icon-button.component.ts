b91f1a6fce4db1e987d2826c02b55fe0b0894a23
KacperAa <kacperaugvstyn@gmail.com>
2024-05-29 15:22:27 +0200

    ref

--- projects/ui/src/lib/molecules/avatar-with-icon-button/avatar-with-icon-button.component.ts
+++ projects/ui/src/lib/molecules/avatar-with-icon-button/avatar-with-icon-button.component.ts
@@ -1,12 +1,10 @@
 import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
-import { MatIcon } from '@angular/material/icon';
 
 import { CircleSizeService } from '../../atoms/circle-size/circle-size.service';
 
 @Component({
   selector: 'ui-avatar-with-icon-button',
   standalone: true,
-  imports: [MatIcon],
   providers: [CircleSizeService],
   styleUrls: ['./avatar-with-icon-button.component.scss'],
   templateUrl: './avatar-with-icon-button.component.html',
