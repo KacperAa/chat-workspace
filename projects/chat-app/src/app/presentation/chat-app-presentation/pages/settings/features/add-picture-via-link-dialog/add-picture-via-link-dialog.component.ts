3a43fad80bdb076664d73c95bb8321dfceebabe9
KacperAa <kacperaugvstyn@gmail.com>
2024-05-30 13:16:45 +0200

    refactor

--- projects/chat-app/src/app/presentation/chat-app-presentation/pages/settings/features/add-picture-via-link-dialog/add-picture-via-link-dialog.component.ts
+++ projects/chat-app/src/app/presentation/chat-app-presentation/pages/settings/features/add-picture-via-link-dialog/add-picture-via-link-dialog.component.ts
@@ -1,4 +1,6 @@
 import { Component, inject } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatButton } from '@angular/material/button';
 import { MatDialogActions, MatDialogContent, MatDialogRef, MatDialogTitle } from '@angular/material/dialog';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
@@ -8,12 +10,24 @@ import { MatInput } from '@angular/material/input';
 @Component({
   selector: 'kaa-add-picture-via-link-dialog',
   standalone: true,
-  imports: [MatFormField, MatInput, MatIcon, MatButton, MatDialogTitle, MatDialogContent, MatDialogActions, MatLabel],
+  imports: [
+    MatIcon,
+    MatLabel,
+    MatInput,
+    MatButton,
+    MatFormField,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    ReactiveFormsModule,
+  ],
   templateUrl: './add-picture-via-link-dialog.component.html',
   styleUrl: './add-picture-via-link-dialog.component.scss',
 })
 export class AddPictureViaLinkDialogComponent {
   private _dialogRef = inject(MatDialogRef);
+
+  public editPictureControl = new FormControl<string | null>('');
 
   onClose(): void {
     this._dialogRef.close();
