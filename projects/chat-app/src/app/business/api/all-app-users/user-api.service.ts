ffc7a56401136a1e3fdba403a79b825e9668dd41
KacperAa <kacperaugvstyn@gmail.com>
2024-06-04 13:16:37 +0200

    implement logic

--- projects/chat-app/src/app/business/api/all-app-users/user-api.service.ts
+++ projects/chat-app/src/app/business/api/all-app-users/user-api.service.ts
@@ -1,9 +1,16 @@
 import { Injectable, inject } from '@angular/core';
-import { Database } from '@firebase/database';
+import { child, get } from '@angular/fire/database';
+import { Database, ref } from '@angular/fire/database';
 import { UserResponse } from 'stream-chat';
 import { ChatClientService, DefaultStreamChatGenerics } from 'stream-chat-angular';
 
 import { Observable, from } from 'rxjs';
+
+interface DatabaseResponse {
+  email: string;
+  photoURL: string;
+  displayName: string;
+}
 
 @Injectable({
   providedIn: 'root',
@@ -14,5 +21,14 @@ export class UserApiService {
 
   public getUsersByFilter(queryString: string): Observable<UserResponse<DefaultStreamChatGenerics>[]> {
     return from(this._chatService.autocompleteUsers(queryString));
+  }
+
+  private _getFireUsersDatabase(uid: string): Observable<DatabaseResponse> {
+    const dbRef = ref(this._fireDatabase);
+    return from(
+      get(child(dbRef, `users/${uid}`)).then(snapshot => {
+        return snapshot.val();
+      })
+    );
   }
 }
