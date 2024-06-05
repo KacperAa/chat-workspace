80534a9e249b447c0a27cb07b0a35dfa7c0e8f3f
KacperAa <kacperaugvstyn@gmail.com>
2024-06-05 09:22:16 +0200

    create add firend service

--- projects/chat-app/src/app/presentation/chat-app-presentation/pages/add-friends-page/main/add-friends-page.component.ts
+++ projects/chat-app/src/app/presentation/chat-app-presentation/pages/add-friends-page/main/add-friends-page.component.ts
@@ -5,6 +5,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 
+import { AddFriendService } from '../../../../../business/api/add-friend/add-friend.service';
 import { UserMergedResponse } from '../../../../../business/api/all-app-users/models/user-merged-response.model';
 import { UserApiService } from '../../../../../business/api/all-app-users/user-api.service';
 import { ChatInitializerService } from '../../../../../business/chat-initializer/chat-initializer.service';
@@ -30,9 +31,9 @@ import { Observable, debounceTime, startWith, switchMap } from 'rxjs';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AddFriendsPageComponent implements OnInit {
-  private _chatInitializer = inject(ChatInitializerService);
-
   private _userApi = inject(UserApiService);
+  private _addFriend = inject(AddFriendService);
+  private _chatInitializer = inject(ChatInitializerService);
 
   public findFiendsControl = new FormControl('', { nonNullable: true });
   public availableUsers$!: Observable<UserMergedResponse[]>;
