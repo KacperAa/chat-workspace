095e5bd1a855a1e9d4fc527310f48b4be19b1377
KacperAa <kacperaugvstyn@gmail.com>
2024-06-11 10:16:01 +0200

    implement skeleton loader

--- projects/chat-app/src/app/presentation/chat-app-presentation/pages/friends-list/main/friends-list-page.component.ts
+++ projects/chat-app/src/app/presentation/chat-app-presentation/pages/friends-list/main/friends-list-page.component.ts
@@ -1,15 +1,27 @@
 import { ChangeDetectionStrategy, Component, OnInit, inject } from '@angular/core';
 import { MatIconButton } from '@angular/material/button';
+import { MatDivider } from '@angular/material/divider';
 import { MatIcon } from '@angular/material/icon';
+import { MatList, MatListModule } from '@angular/material/list';
 import { MatToolbar } from '@angular/material/toolbar';
 
+import { SkeletonBarComponent, SkeletonCircleLoaderComponent } from '../../../../../../../../ui/src/lib/atoms';
 import { FriendsListComponent } from '../ui/organisms/friends-list/friends-list.component';
 import { FriendsListPageFacade } from './friends-list-page.facade';
 
 @Component({
   selector: 'kaa-firends-list-page',
   standalone: true,
-  imports: [MatToolbar, MatIconButton, MatIcon, FriendsListComponent],
+  imports: [
+    MatToolbar,
+    MatIconButton,
+    MatIcon,
+    FriendsListComponent,
+    SkeletonCircleLoaderComponent,
+    SkeletonBarComponent,
+    MatListModule,
+    MatDivider,
+  ],
   providers: [FriendsListPageFacade],
   templateUrl: './friends-list-page.component.html',
   styleUrl: './friends-list-page.component.scss',
