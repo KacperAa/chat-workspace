86b795ac6fb2afcf1acbbcf9a6c581138235d5cc
KacperAa <kacperaugvstyn@gmail.com>
2024-06-12 14:25:53 +0200

    refactor

--- projects/chat-app/src/app/business/api/channels/channels-api.service.ts
+++ projects/chat-app/src/app/business/api/channels/channels-api.service.ts
@@ -2,7 +2,7 @@ import { Injectable, inject } from '@angular/core';
 import { Channel } from 'stream-chat';
 import { ChannelService, DefaultStreamChatGenerics } from 'stream-chat-angular';
 
-import { Observable, filter, map, shareReplay } from 'rxjs';
+import { Observable, filter, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -12,7 +12,6 @@ export class ChannelsApiService {
 
   public getChannelApi(id: string): Observable<Channel<DefaultStreamChatGenerics>> {
     return this._channelService.channels$.pipe(
-      shareReplay(),
       map(channels => channels?.find(channel => channel.id === id)),
       filter((channel): channel is Channel<DefaultStreamChatGenerics> => !!channel)
     );
