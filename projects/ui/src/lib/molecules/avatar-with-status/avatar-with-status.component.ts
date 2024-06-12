07b1dc3f52f230fc215a6821f2ebebf0c53b7bea
KacperAa <kacperaugvstyn@gmail.com>
2024-06-12 09:57:50 +0200

    refactor

--- projects/ui/src/lib/molecules/avatar-with-status/avatar-with-status.component.ts
+++ projects/ui/src/lib/molecules/avatar-with-status/avatar-with-status.component.ts
@@ -1,10 +1,9 @@
-import { NgClass } from '@angular/common';
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 
 @Component({
   selector: 'ui-avatar-with-status',
   standalone: true,
-  imports: [NgClass],
+  imports: [],
   templateUrl: './avatar-with-status.component.html',
   styleUrl: './avatar-with-status.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
