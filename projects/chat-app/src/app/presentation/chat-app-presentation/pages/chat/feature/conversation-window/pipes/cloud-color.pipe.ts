bc1895f37a211f3f6f91b184bce887efadc47a45
KacperAa <kacperaugvstyn@gmail.com>
2024-06-17 13:11:53 +0200

    refactor

---
+++ projects/chat-app/src/app/presentation/chat-app-presentation/pages/chat/feature/conversation-window/pipes/cloud-color.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+import { TextCloudColor } from '../../../../../../../../../../ui/src/lib/atoms/text-cloud/models/text-cloud-color.model';
+
+@Pipe({
+  name: 'cloudColor',
+  standalone: true,
+})
+export class CloudColorPipe implements PipeTransform {
+  transform(isCurrentUser: boolean): TextCloudColor {
+    return isCurrentUser ? 'green' : 'darkgray';
+  }
+}
