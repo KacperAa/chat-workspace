9fc81a2fe7cd636cfb680cfd12b89fa755c1a554
KacperAa <kacperaugvstyn@gmail.com>
2024-06-17 13:27:05 +0200

    finish text cloud position

--- projects/ui/src/lib/atoms/text-cloud/text-cloud.component.ts
+++ projects/ui/src/lib/atoms/text-cloud/text-cloud.component.ts
@@ -1,15 +1,18 @@
+import { NgClass } from '@angular/common';
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 
 import { TextCloudColor } from './models/text-cloud-color.model';
+import { TextCloudPosition } from './models/text-cloud-position.model';
 
 @Component({
   selector: 'ui-text-cloud',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './text-cloud.component.html',
   styleUrl: './text-cloud.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TextCloudComponent {
   public color = input<TextCloudColor>('darkgray');
+  public position = input<TextCloudPosition>('left');
 }
