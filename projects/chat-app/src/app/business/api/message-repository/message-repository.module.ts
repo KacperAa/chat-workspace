852c45b7ddef3115ba43b4575176c91b50076e52
KacperAa <kacperaugvstyn@gmail.com>
2024-06-17 12:42:09 +0200

    create mepper type

--- projects/chat-app/src/app/business/api/message-repository/message-repository.module.ts
+++ projects/chat-app/src/app/business/api/message-repository/message-repository.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 
 import { MessageApiService } from './message-api.service';
+import { MessageMapperService } from './message-mapper/message-mapper.service';
 import { SendMessageApiService } from './send-message/send-message-api.service';
 
-@NgModule({ providers: [MessageApiService, SendMessageApiService] })
+@NgModule({ providers: [MessageApiService, SendMessageApiService, MessageMapperService] })
 export class MessageRepositoryModule {}
