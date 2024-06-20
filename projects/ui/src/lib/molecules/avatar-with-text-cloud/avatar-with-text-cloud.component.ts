815c08e7b7e2555422e7676fd76af4274d0cbea2
KacperAa <kacperaugvstyn@gmail.com>
2024-06-20 15:32:32 +0200

    ref

--- projects/ui/src/lib/molecules/avatar-with-text-cloud/avatar-with-text-cloud.component.ts
+++ projects/ui/src/lib/molecules/avatar-with-text-cloud/avatar-with-text-cloud.component.ts
@@ -1,10 +1,20 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import {
+  AfterContentChecked,
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  contentChild,
+  input,
+  viewChild,
+} from '@angular/core';
 
 import { CloudColorPipe } from '../../../../../chat-app/src/app/presentation/chat-app-presentation/pages/chat/feature/conversation-window/pipes/cloud-color.pipe';
 import { CloudPositionPipe } from '../../../../../chat-app/src/app/presentation/chat-app-presentation/pages/chat/feature/conversation-window/pipes/cloud-position.pipe';
+import { AvatarComponent } from '../../atoms/avatar/avatar.component';
 import { TextCloudColor } from '../../atoms/text-cloud/models/text-cloud-color.model';
 import { TextCloudPosition } from '../../atoms/text-cloud/models/text-cloud-position.model';
 import { TextCloudComponent } from '../../atoms/text-cloud/text-cloud.component';
+import { getMissingImageError, getMissingSpanError } from './errors/avatar-with-text-cloud-errors';
 
 @Component({
   selector: 'ui-avatar-with-text-cloud',
@@ -14,7 +24,27 @@ import { TextCloudComponent } from '../../atoms/text-cloud/text-cloud.component'
   imports: [TextCloudComponent, CloudColorPipe, CloudPositionPipe],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AvatarWithTextCloudComponent {
+export class AvatarWithTextCloudComponent implements AfterContentChecked {
+  private _avatarComponent = contentChild<AvatarComponent | undefined>(AvatarComponent);
+  private _spanElementContainer = viewChild.required<ElementRef<HTMLElement>>('spanElementContainer');
+
   public color = input<TextCloudColor>('darkgray');
   public position = input<TextCloudPosition>('right');
+
+  public ngAfterContentChecked(): void {
+    this._throwMissingSpanError();
+    this._throwMissingImageError();
+  }
+
+  private _throwMissingSpanError() {
+    if (this._spanElementContainer().nativeElement.childNodes.length === 0) {
+      throw getMissingSpanError();
+    }
+  }
+
+  private _throwMissingImageError() {
+    if (!this._avatarComponent()) {
+      throw getMissingImageError();
+    }
+  }
 }
