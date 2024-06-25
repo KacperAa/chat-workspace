af4060fe6613435e9e2a213dc3bdfe7a877fe1c3
KacperAa <kacperaugvstyn@gmail.com>
2024-06-25 09:40:58 +0200

    finish watch status service

--- projects/chat-app/src/app/business/api/channel-repository/channel-watch-status/channel-watch-status.service.ts
+++ projects/chat-app/src/app/business/api/channel-repository/channel-watch-status/channel-watch-status.service.ts
@@ -1,5 +1,4 @@
 import { Injectable, inject } from '@angular/core';
-import { Auth } from '@angular/fire/auth';
 
 import { ChannelsApiService } from '../channels-api.service';
 
@@ -8,9 +7,6 @@ import { Observable, map } from 'rxjs';
 @Injectable()
 export class ChannelWatchStatusService {
   private _channelApi = inject(ChannelsApiService);
-  private _auth = inject(Auth);
-
-  private _currentUserUid = this._auth.currentUser?.uid;
 
   public markAllChannelsAsStopWatching(): Observable<void> {
     return this._channelApi.getChannelsApi().pipe(map(channels => channels.forEach(channel => channel.stopWatching())));
